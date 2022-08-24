f958fe206d0dcc919e82d5ed2e3f916bcc30dcf9
Izar Mula Soria <ThemulaitorIMS@gmail.com>
2022-08-24 20:17:32 +0200

    adding form component
    Adding form component

---
+++ src/components/FormComponent.js
@@ -0,0 +1,15 @@
+import '../styles/Form.scss';
+
+const FormComponent = () => {
+    return (
+        <div className="basicForm">
+            <h2>Article Register</h2>
+            <p>
+                Add articles and {''}
+                <span className='alterFormText'>Manage them</span>
+            </p>
+        </div>
+    );
+}
+
+export default FormComponent;
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import '../styles/Header.scss';
+
+const HeaderComponent = () => {
+    return (
+        <div className="headerMain">
+            <h2>Simple {''}</h2>
+            <h2 className="alterTitleText">Store</h2>
+            <h6>By Izar Mula</h6>
+        </div>
+    );
+}
+
+export default HeaderComponent;
